701871abd97d58e1c717cf097da69e85ec39c43a
Ilhom77 <103019100+Ilhom77@users.noreply.github.com>
2022-05-01 13:23:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,47 +1,54 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-double pown(double value, uint16_t n) {
-  double x = 1;
-    for (uint16_t i = 1; i <= n; i++) {
-        x = x * value;
+
+
+double pown(double value, uint16_t z) {
+if (z == 0) {
+      return 1;
     }
-    return x;
+    if (z == 1) {
+        return value;
+    } else {
+        return value * (pown(value, z - 1));
+    }
 }
-uint64_t fact(uint16_t n) {
-  uint64_t x = 1;
-    if (n == 1) {
+uint64_t fact(uint16_t z) {
+if (z <= 1) {
         return 1;
     } else {
-        for (uint16_t i = 1; i <= n; i++) {
-            x = x * i;
-        }
+        return z * fact(z - 1);
     }
-    return x;
 }
-double calcItem(double x, uint16_t n) {
-  double f = fact(n);
-    double st = pown(x, n);
-    double res = st / f;
-    return (res);
+double calcItem(double x, uint16_t z) {
+  return (pown(x, z) / fact(z));
 }
-double expn(double x, uint16_t count) 
-  double z = 1;
-    for (uint16_t i = 1; i <= count; i++) {
-        z = z + calcItem(x, i);
+double expn(double x, uint16_t count) {
+  double result_ex = 1;
+    for (int z = count; z > 0; z--) {
+        result_ex += pown(x, z) / fact(z);
     }
-    return z;
+    return result_ex;
 }
 double sinn(double x, uint16_t count) {
-  double z = 0;
-    for (uint16_t i = 1; i <= count; i++) {
-        z = z + (pown(-1, i - 1) * calcItem(x, 2 * i - 1));
+if (count == 1) {
+        return x;
     }
-    return z;
+    double result_sin = 0;
+    for (int z = count; z > 0; z--) {
+        result_sin += (pown((-1), z - 1))*((pown(x, 2 * z - 1))
+            /(fact(2 * z - 1)));
+    }
+    return result_sin;
 }
 double cosn(double x, uint16_t count) {
-  double z = 0;
-    for (uint16_t i = 1; i <= count; i++) {
-        z = z + (pown(-1, i - 1) * calcItem(x, 2 * i - 2));
+double result_cos = 0;
+    if (count == 1) {
+        return x;
     }
-    return z;
+    for (int z = count; z > 0; z--) {
+        result_cos += (pown((-1), z - 1))*((pown(x, 2 * z - 2))
+            /(fact(2 * z - 2)));
+    }
+    return result_cos;
 }
